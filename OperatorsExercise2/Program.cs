14bf17cf4acce3cf1d9769d7975dd10e7439a09f
Damion Russell <domino.russell@gmail.com>
2022-07-16 20:09:40 -0400

    initial commit

---
+++ OperatorsExercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The are of a circle with radius of {radius} is {AreaOfCircle(radius)}");
+            Console.ReadLine(); // This is just to keep the console from closing right away
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
